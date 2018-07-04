ebe5a951163674935563028fb4da47318a093497
Ubuntu <ubuntu@ip-172-31-27-95.us-west-2.compute.internal>
2018-07-04 20:22:06 +0000

    latest

--- src/ipynb/case-study-01-seeds/visualization.py
+++ src/ipynb/case-study-01-seeds/visualization.py
@@ -12,18 +12,17 @@ def scatter_plot_by_class(ax, features, target, feat_1='Component_1', feat_2='Co
         mask = target == cls
         features[mask].plot(x=feat_1, y=feat_2, label=species[i], kind='scatter', ax=ax, color=color)
         
-def scatter_plot_with_decision_boundary(ax, features, target, feat_1, feat_2):
+def scatter_plot_with_decision_boundary(ax, features, target, feat_1, feat_2, model):
     X = features[[feat_1, feat_2]]
     y = target
     h = .02  # step size in the mesh
 
-    logreg = LogisticRegression()
-    logreg.fit(X, y)
+    model.fit(X, y)
 
     x_min, x_max = X[feat_1].min() - .5, X[feat_1].max() + .5
     y_min, y_max = X[feat_2].min() - .5, X[feat_2].max() + .5
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-    Z = logreg.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
 
     scatter_plot_by_class(ax, features, target, feat_1, feat_2)
     Z = Z.reshape(xx.shape)
